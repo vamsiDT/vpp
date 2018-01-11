c5a40899543018593d502386420fac366518422d
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-01-11 14:13:37 +0100

    update

---
+++ vpp/src/vnet/ip/fifo.h
@@ -0,0 +1,25 @@
+#include <vlib/vlib.h>
+#include <vnet/ip/ip.h>
+#include <vnet/vnet.h>
+#include <stdlib.h>
+#include <math.h>
+#ifndef FIFO_H
+#define FIFO_H
+#define ALPHA 1.0
+extern u64 old_t;
+extern u64 t;
+extern u32 threshold;
+extern u32 fifoqueue;
+
+always_inline u8 fifo(u16 pktlen){
+	u8 drop;
+	if(fifoqueue <= threshold ){
+		fifoqueue+=pktlen;
+        drop=0;
+    }
+	else
+		drop=1;
+return drop;
+}
+
+#endif /*FIFO_H*/
---
+++ vpp/src/vnet/ip/fifo_var.h
@@ -0,0 +1,6 @@
+#include "vnet/ip/fifo.h"
+
+u64 old_t;
+u64 t = 0;
+u32 fifoqueue = 0;
+u32 threshold;
