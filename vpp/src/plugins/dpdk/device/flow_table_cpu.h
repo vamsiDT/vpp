77d249b4c9ce35e4e2578777f1e79776fda0e656
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-02-02 18:16:16 +0100

    update

--- vpp/src/plugins/dpdk/device/flow_table_cpu.h
+++ vpp/src/plugins/dpdk/device/flow_table_cpu.h
@@ -30,11 +30,11 @@
 #ifdef ELOG_FAIRDROP
 #define WEIGHT_DPDK 208
 #else
-#define WEIGHT_DPDK 180//185
+#define WEIGHT_DPDK 160//185
 #endif
 
 #define WEIGHT_IP4E 192
-#define WEIGHT_CLASS_1 372
+#define WEIGHT_CLASS_1 352
 #define WEIGHT_CLASS_2 (WEIGHT_DPDK+WEIGHT_IP4E)
 
 #ifdef BUSYLOOP
@@ -407,7 +407,7 @@ always_inline void vstate(flowcount_t * flow,u8 update,u32 cpu_index){
 #else	/*Exact value of credit calculation in which the clock cycles spent in dropping the packets is subtracted. */
 		credit = (((t[cpu_index]-old_t[cpu_index])) - (n_drops[cpu_index]*(error_cost[cpu_index]+dpdk_cost_total[cpu_index])));
 #endif
-		threshold[cpu_index] = (credit*((f32)(4)))/nbl[cpu_index];//(credit)*2;//((f32)n_packets)*((f32)380.0)/nbl[cpu_index];
+		threshold[cpu_index] = (credit*((f32)(1.2)))/nbl[cpu_index];//(credit)*2;//((f32)n_packets)*((f32)380.0)/nbl[cpu_index];
 		//printf("%f\n",threshold[cpu_index]);
 		//veryold_t[cpu_index] = nbl[cpu_index];
         // printf("%u\n",nbl[cpu_index]);
@@ -510,6 +510,7 @@ always_inline void update_costs(u32 cpu_index){
 	while(n>0){
 		flow0 = costlist->flow;
 		flow0->cost = flow0->weight*(total/su);
+		//printf("%u\n",flow0->cost);
 //		flow0->vqueue = 1;
 		costlist = costlist->next;
 		n -= 1;
