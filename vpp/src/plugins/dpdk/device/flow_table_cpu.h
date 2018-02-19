946fab976e96bf1b7e7083c8ced8c26fb98bc4f7
Vamsi <f2013790@goa.bits-pilani.ac.in>
2018-02-19 05:42:10 +0100

    Update flow_table_cpu.h

--- vpp/src/plugins/dpdk/device/flow_table_cpu.h
+++ vpp/src/plugins/dpdk/device/flow_table_cpu.h
@@ -171,7 +171,7 @@ extern u8 hello_world[MAXCPU];
 extern u64 s[MAXCPU];
 extern u64 s_total[MAXCPU];
 extern u32 busyloop[MAXCPU];
-extern f64 sum[MAXCPU];
+extern u64 sum[MAXCPU];
 extern u64 dpdk_cost_total[MAXCPU];
 
 extern f32 threshold[MAXCPU];
@@ -370,11 +370,11 @@ always_inline void update_costs(u32 cpu_index){
     if (PREDICT_TRUE(costlist->flow != NULL)){
         flowcount_t * flow0;
         f64 total = (f64)s_total[cpu_index];
-        f64 su = (f64)sum[cpu_index];
+        f64 su = sum[cpu_index];
         u32 n = nbl[cpu_index];
     while(n>0){
         flow0 = costlist->flow;
-        flow0->cost = flow0->weight*(total/su);
+        flow0->cost = total/(su/flow0->weight);
         costlist = costlist->next;
         n -= 1;
     }
