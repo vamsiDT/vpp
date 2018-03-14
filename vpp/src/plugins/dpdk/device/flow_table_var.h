b60b3441c5ff772efa6e743c09b03007dbe63af5
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-03-14 02:57:19 +0100

    update

--- vpp/src/plugins/dpdk/device/flow_table_var.h
+++ vpp/src/plugins/dpdk/device/flow_table_var.h
@@ -1,16 +1,16 @@
 #include <dpdk/device/flow_table.h>
-flowcount_t *  nodet[TABLESIZE] ;
+flowcount_t *  nodet[TABLESIZE][NETINT] ;
 activelist_t * head_af;
 activelist_t * tail_af;
-flowcount_t *  head ;
+flowcount_t *  head[NETINT] ;
 flowcount_t *  previousnode;
 flowcount_t *  tail;
 int numflows;
 u32 r_qtotal;
-u32 nbl;
-u64 t = 0;
-u64 old_t;
-f32 threshold;
+u32 nbl[NETINT];
+u64 t[NETINT]= {0,0};
+u64 old_t[NETINT];
+f32 threshold[NETINT];
 activelist_t * act;
-activelist_t * head_act;
-activelist_t * tail_act;
+activelist_t * head_act[NETINT];
+activelist_t * tail_act[NETINT];
